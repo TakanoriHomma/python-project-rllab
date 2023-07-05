e8842b8ec568be12da0c104f7c24d5e51050280d
TakanoriHomma <takasama_1205@yahoo.co.jp>
2023-07-05 17:32:09 +0900

    revising dockerfile, build file, and run file

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,9 +4,10 @@ PARENT_DIR=$(pwd|xargs dirname)
 
 docker run --name kadai3_vae -it --rm -d --gpus all --ipc=host \
            --ulimit memlock=-1 --ulimit stack=67108864 \
-           -v /tmp/.X11-unix:/tmp/.X11-unix: -v "${PARENT_DIR}":/home/"${USER}"/Kadai3vae \
+           -v /tmp/.X11-unix:/tmp/.X11-unix: \
+           -v "${PARENT_DIR}":/home/"${USER}"/python-project-rllab \
            -e DISPLAY="${DISPLAY}" \
            -p 63322:63322 \
            -p 6006:6006 \
            -p 6007:6007 \
-           kadai3_vae jupyter lab --config='./Kadai3vae/jupyter_lab_config.py'
+           kadai3_vae jupyter lab --config='./python-project-rllab/jupyter_lab_config.py'
